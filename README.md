ed96592e90f83c89ffb5be39d7ff0b937656934c
Ubirajara Rodrigues <ubirajararodrigues@outlook.com>
2020-07-08 22:45:54 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,9 @@
+# webapi-sample
+
+Implementação parcial e com modificações do tutorial de WebApi da documentação oficial do .netcore.
+
+Link do tutorial:
+
+https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-3.1&tabs=visual-studio-code
+
+Esta implmentação foi feita apenas para estudo.
---
+++ Utils/ConvertToDTO.cs
@@ -0,0 +1,17 @@
+using TodoApi.Dtos;
+using TodoApi.Models;
+
+namespace TodoApi.Utils
+{
+    public static class ConvertToDTO
+    {
+        public static TodoItemDTO TodoItemDTO(TodoItem todoItem) =>
+         new TodoItemDTO
+         {
+             Id = todoItem.Id,
+             Name = todoItem.Name,
+             IsComplete = todoItem.IsComplete
+         };
+
+    }
+}
